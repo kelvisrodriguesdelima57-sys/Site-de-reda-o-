269af46219e02ddceebe20c4d2ed3bb2ad77b457
kelvisrodriguesdelima57-sys <kelvisrodriguesdelima57@gmail.com>
2025-09-14 13:33:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const speedInsights = require('@vercel/speed-insights');
 
-async function testarSite(url) {
-    const result = await speedInsights.run(url);
-        console.log(result);
-        }
-
-        // Troque a URL pelo site que você quer testar
-        testarSite('https://gerador-de-reda-os-b34f.vercel.app/
